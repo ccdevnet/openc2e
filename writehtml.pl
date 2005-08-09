80d20c3ab4a436af8c3e14a68c83a1c0b6342a04
bdonlan <bdonlan@5a3b5b51-85f2-0310-b56e-886e3cf1dda3>
2005-08-09 23:59:53 +0000

    categorization of commands
    git-svn-id: svn://openc2e.ccdevnet.org/openc2e/branches/newvm@257 5a3b5b51-85f2-0310-b56e-886e3cf1dda3

--- writehtml.pl
+++ writehtml.pl
@@ -6,6 +6,31 @@ use warnings;
 use YAML;
 use Template;
 
+sub captext ($);
+
+BEGIN {
+	eval {
+		require Text::Capitalize;
+		import Text::Capitalize;
+	};
+	if ($@) {
+		*captext = sub ($) {
+			my $t = shift;
+			$t =~ s/^([a-z])/uc $1/e;
+		};
+	} else {
+		*captext = sub ($) {
+			return capitalize_title($_[0]);
+		};
+	}
+}
+
+sub esc ($) {
+	my $t = shift;
+	$t =~ s/\W/_/g;
+	return $t;
+}
+
 my $data = YAML::LoadFile('commandinfo.yml');
 
 foreach my $key (keys %{$data->{ops}}) {
@@ -20,8 +45,24 @@ foreach my $key (keys %{$data->{ops}}) {
    }
 }
 
+my %catsort;
+foreach my $key (sort keys %{$data->{ops}}) {
+	push @{$catsort{$data->{ops}{$key}{category}}}, $data->{ops}{$key};
+}
+
+foreach my $key (keys %catsort) {
+	$catsort{$key} = [
+		map { $_->[0] }
+		sort { $a->[1] cmp $b->[1] }
+		map { [ $_, $_->{name} ] }
+		@{$catsort{$key}}
+	];
+}
+
+my @catl = map { { name => captext($_), ents => $catsort{$_}, anchor => esc($_) } } sort keys %catsort;
+
 my $vars = {
-	commands => [map {$data->{ops}{$_}} sort keys %{$data->{ops}}]
+	categories => \@catl
 };
 
 my $tt = new Template;
@@ -35,70 +76,82 @@ __END__
 	</head>
 	<body>
 		<h1>CAOS command reference - openc2e dev build</h1>
-		[% FOREACH op = commands %]
-		<div class="command" id="c_[% op.key %]">
-			<div class="header">
-				<span class="cmdname">
-					[% op.name %]
-				</span>
-				<span class="cmdtype">
-					([% op.type %])
-				</span>
-				[% FOREACH arg = op.arguments -%]
-				<span class="argname">
-					[% arg.name %]
-				</span>
-				<span class="argtype">
-					([% arg.type %])
-				</span>
-				[% END %]
-			</div>
-			<div class="description">
-			[% UNLESS op.hasdocs %]
-			<div class="nodocs">This command is currently undocumented.</div>
-			[% ELSE %]
-			<div class="docs">
-				[% op.description %]
-			</div>
+		<div id="index"><ul>
+			[% FOREACH cat = categories %]
+			<li><a href="#c_[%- cat.anchor -%]">[% cat.name %]</a></li>
 			[% END %]
-			</div>
-			<div class="status">
-				[% IF op.status == 'todo' || op.status == 'stub' %]
-				<div class="st_todo">
-					This command is not yet implemented.
+		</ul></div>
+		[% FOREACH cat = categories %]
+		<div class="category" id="c_[%- cat.anchor -%]">
+			<h2>[% cat.name %]</h2><hr/>
+			<!-- XXX: hr is not semantic -->
+
+			[% FOREACH op = cat.ents %]
+			<div class="command" id="k_[% op.key %]">
+				<div class="header">
+					<span class="cmdname">
+						[% op.name %]
+					</span>
+					<span class="cmdtype">
+						([% op.type %])
+					</span>
+					[% FOREACH arg = op.arguments -%]
+					<span class="argname">
+						[% arg.name %]
+					</span>
+					<span class="argtype">
+						([% arg.type %])
+					</span>
+					[% END %]
 				</div>
-				[% ELSIF op.status == 'probablyok' %]
-				<div class="st_maybe">
-					This command probably works, but it has not been annotated with its status.
-				</div>
-				[% ELSIF op.status == 'ok' || op.status == 'done' %]
-				<div class="st_ok">
-					This command works properly.
-				</div>
+				<div class="description">
+				[% UNLESS op.hasdocs %]
+				<div class="nodocs">This command is currently undocumented.</div>
 				[% ELSE %]
-				<div class="st_wtf">
-					This command has an unknown status tag of: [% op.status %]. Please beat whoever set that status with a shovel or some other heavy object.
+				<div class="docs">
+					[% op.description %]
 				</div>
 				[% END %]
-			</div>	
+				</div>
+				<div class="status">
+					[% IF op.status == 'todo' || op.status == 'stub' %]
+					<div class="st_todo">
+						This command is not yet implemented.
+					</div>
+					[% ELSIF op.status == 'probablyok' %]
+					<div class="st_maybe">
+						This command probably works, but it has not been annotated with its status.
+					</div>
+					[% ELSIF op.status == 'ok' || op.status == 'done' %]
+					<div class="st_ok">
+						This command works properly.
+					</div>
+					[% ELSE %]
+					<div class="st_wtf">
+						This command has an unknown status tag of: [% op.status %]. Please beat whoever set that status with a shovel or some other heavy object.
+					</div>
+					[% END %]
+				</div>	
 
-			<div class="administrivia">
-				<ul>
-					<li>Implemented in: [% op.filename %]</li>
-					<li>Implementation function (may be wrong):
-						[% op.implementation %]
-					</li>
-					[% IF op.pragma %]
-					<li>Pragmas:
+				<div class="administrivia">
 					<ul>
-						[% FOREACH pragma = op.pragma %]
-						<li> [% pragma.key %] =&gt; [% pragma.value %] </li>
+						<li>Implemented in: [% op.filename %]</li>
+						<li>Implementation function (may be wrong):
+							[% op.implementation %]
+						</li>
+						[% IF op.pragma %]
+						<li>Pragmas:
+						<ul>
+							[% FOREACH pragma = op.pragma %]
+							<li> [% pragma.key %] =&gt; [% pragma.value %] </li>
+							[% END %]
+						</ul>
 						[% END %]
 					</ul>
-					[% END %]
-				</ul>
-			</div>
-		</div> <!-- command -->
+				</div>
+			</div> <!-- command -->
+			[% END %]
+		</div> <!-- category -->
 		[% END %]
 	</body>
 </html>
