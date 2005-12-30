cc08748d3c1f4f7027b9aae6e561f88f94b61f19
Bryan Donlan <bdonlan@gmail.com>
2005-12-30 03:39:19 +0000

    Fix serialization for ubuntu, I hope (branch)

--- serialization.h
+++ serialization.h
@@ -1,9 +1,13 @@
 #ifndef SERIALIZATION_H
 #define SERIALIZATION_H 1
 
+
+// This order is important
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
 #include <boost/serialization/version.hpp>
 #include <boost/serialization/split_member.hpp>
-#include <boost/serialization/export.hpp>
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/utility.hpp>
 #include <boost/serialization/list.hpp>
@@ -12,6 +16,8 @@
 #include <boost/serialization/set.hpp>
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/string.hpp>
+
+#include <boost/serialization/export.hpp>
 
 #define SER_BASE(ar,bc) \
     do { ar & boost::serialization::base_object<bc>(*this); } while (0)
