715b985e373e64523db0e970bbf02df35c92bc3f
nornagon <nornagon@5a3b5b51-85f2-0310-b56e-886e3cf1dda3>
2007-08-04 02:05:45 +0000

    hacky support for non-c3 unit tests
    git-svn-id: svn://openc2e.ccdevnet.org/openc2e/branches/newvm2@1249 5a3b5b51-85f2-0310-b56e-886e3cf1dda3

--- runtests.pl
+++ runtests.pl
@@ -8,11 +8,24 @@ my @tests = glob("unittests/*.cos");
 for my $test (@tests) {
     my $wrapper = $test;
     $wrapper =~ s/cos$/pl/;
+		my @variants = ();
+		if ($wrapper =~ /\.((c[123av])(,c[123av])*)\.pl$/) {
+			for my $v (split /,/, $1) {
+				push @variants,$v;
+			}
+		}
+		if (@variants == 0) {
+			@variants = ('c3');
+		}
 
+		my $varr = '("'.join("\",\"",@variants).'")';
+		#$varr = "(".$varr.")";
     open FILE, ">$wrapper";
     print FILE <<EOF;
 #!/usr/bin/perl
-exec("./openc2e", "-n", "-b", "$test");
+for my \$v ($varr) {
+	exec("./openc2e", "-n", "-d", "\$v"."data", "-g", "\$v", "-b", "$test");
+}
 EOF
     close FILE;
     chmod 0700, $wrapper;
