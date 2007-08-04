7a1a908719b4ea61e01bd3f9882a50efffff7b3f
nornagon <nornagon@5a3b5b51-85f2-0310-b56e-886e3cf1dda3>
2007-08-04 02:25:03 +0000

    saner runtests.pl
    git-svn-id: svn://openc2e.ccdevnet.org/openc2e/branches/newvm2@1252 5a3b5b51-85f2-0310-b56e-886e3cf1dda3

--- runtests.pl
+++ runtests.pl
@@ -4,12 +4,11 @@ use warnings;
 use Test::Harness qw(&runtests $verbose);
 
 my @tests = glob("unittests/*.cos");
+my @testfiles = ();
 
 for my $test (@tests) {
-    my $wrapper = $test;
-    $wrapper =~ s/cos$/pl/;
 		my @variants = ();
-		if ($wrapper =~ /\.((c[123av])(,c[123av])*)\.pl$/) {
+		if ($test =~ /\.((c[123av])(,c[123av])*)\.cos$/) {
 			for my $v (split /,/, $1) {
 				push @variants,$v;
 			}
@@ -18,21 +17,21 @@ for my $test (@tests) {
 			@variants = ('c3');
 		}
 
-		my $varr = '("'.join("\",\"",@variants).'")';
-		#$varr = "(".$varr.")";
-    open FILE, ">$wrapper";
-    print FILE <<EOF;
+		#my $varr = '("'.join("\",\"",@variants).'")';
+		for my $v (@variants) {
+			my $wrapper = $test;
+			$wrapper =~ s/(\.[c123av,]*)?\.cos$/.$v.pl/;
+			open FILE, ">$wrapper";
+			print FILE <<EOF;
 #!/usr/bin/perl
-for my \$v ($varr) {
-	exec("./openc2e", "-n", "-d", "\$v"."data", "-g", "\$v", "-b", "$test");
-}
+exec("./openc2e", "-n", "-d", "$v"."data", "-g", "$v", "-b", "$test");
 EOF
-    close FILE;
-    chmod 0700, $wrapper;
-
-    $test = $wrapper
+			close FILE;
+			chmod 0700, $wrapper;
+			push @testfiles,$wrapper;
+		}
 }
 
 $verbose = 0;
-runtests(@tests);
-map { unlink } @tests;
+runtests(@testfiles);
+map { unlink } @testfiles;
